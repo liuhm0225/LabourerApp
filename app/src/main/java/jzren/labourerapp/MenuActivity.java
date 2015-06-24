c649c3b284d08971dd0d06ffd4b057bc675c7eac
liuhaiming <michael3260@sina.com>
2015-06-24 23:54:20 +0800

    修改menu界面

--- app/src/main/java/jzren/labourerapp/MenuActivity.java
+++ app/src/main/java/jzren/labourerapp/MenuActivity.java
@@ -1,10 +1,20 @@
 package jzren.labourerapp;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MenuActivity extends ActionBarActivity {
@@ -13,6 +23,41 @@ public class MenuActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        initMenu();
+    }
+
+    private void initMenu() {
+        GridView gridview = (GridView) findViewById(R.id.GridView);
+        List<Map<String, Object>> meumList = new ArrayList< Map<String, Object> >();
+        for(int i = 1;i < 10;i++)
+        {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", R.mipmap.ic_launcher);
+            map.put("ItemText", ""+i);
+            meumList.add(map);
+        }
+        SimpleAdapter saItem = new SimpleAdapter(this,
+                meumList,
+                R.layout.menuitem,
+                new String[]{"ItemImage","ItemText"},
+                new int[]{R.id.menuItemImage,R.id.menuItemName});
+
+
+        gridview.setAdapter(saItem);
+
+        gridview.setOnItemClickListener(
+                new AdapterView.OnItemClickListener()
+                {
+                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3)
+                    {
+                        int index=arg2+1;//id是从0开始的，所以需要+1
+                        Toast.makeText(getApplicationContext(), "你按下了选项：" + index, Toast.LENGTH_SHORT).show();
+                        //Toast用于向用户显示一些帮助/提示
+                    }
+                }
+        );
+
     }
 
     @Override
